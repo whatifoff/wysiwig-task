a0b882dcbd2f29faf2fa759c509e7974c509e986
Nikita Khakhalev <whatifoff@gmail.com>
2021-01-31 18:25:45 +0300

    Второй вариант

--- js/handlers.js
+++ js/handlers.js
@@ -1,15 +1,85 @@
+const PARENT_TAG = 'PRE'
+
+const getParentElement = () => {
+    return document.getSelection().anchorNode.parentElement
+}
+
+const getParentElementTag = () => {
+    const parentElement = getParentElement()
+
+    if (!parentElement) return
+
+    return parentElement.tagName.toUpperCase()
+}
+
+const getClassList = () => {
+    const parentElement = getParentElement()
+
+    if (!parentElement) return
+
+    return parentElement.classList
+}
+
+const insertFirstClass = classChange => {
+    document.execCommand(
+        'insertHTML',
+        false,
+        `<${PARENT_TAG} class="${classChange}">${document
+            .getSelection()
+            .toString()}</${PARENT_TAG}>`
+    )
+}
+
+const toggleClass = classChange => {
+    const classList = getClassList()
+
+    if (classList.contains(classChange)) {
+        classList.remove(classChange)
+    } else {
+        classList.add(classChange)
+    }
+}
+
+const removeClass = classChange => {
+    const classList = getClassList()
+
+    if (classList.contains(classChange)) {
+        classList.remove(classChange)
+    }
+}
+
+const formatSelection = classChange => {
+    const parentElementTagName = getParentElementTag()
+
+    if (parentElementTagName === PARENT_TAG) {
+        toggleClass(classChange)
+    } else {
+        insertFirstClass(classChange)
+    }
+}
+
 clickButtonBold = e => {
-    document.execCommand('bold')
+    const classChange = CSS_CLASSES.BOLD
+    formatSelection(classChange)
 }
 
 clickButtonH1 = e => {
-    document.execCommand('formatBlock', false, 'h1')
+    const classChange = CSS_CLASSES.H1
+
+    removeClass(CSS_CLASSES.H2)
+
+    formatSelection(classChange)
 }
 
 clickButtonH2 = e => {
-    document.execCommand('formatBlock', false, 'h2')
+    const classChange = CSS_CLASSES.H2
+
+    removeClass(CSS_CLASSES.H1)
+
+    formatSelection(classChange)
 }
 
 clickButtonItalic = e => {
-    document.execCommand('italic')
+    const classChange = CSS_CLASSES.ITALIC
+    formatSelection(classChange)
 }
---
+++ js/struct/css-classes.js
@@ -0,0 +1,6 @@
+const CSS_CLASSES = {
+    H1: 'header1-text',
+    H2: 'header2-text',
+    BOLD: 'bold-text',
+    ITALIC: 'italic-text'
+}
